c62dc8b25244d5ee7a75c88745d6698d2f0e7729
David Fury <david.fury@reservoir-devs.com>
2020-03-22 15:25:38 +0000

    feat: added in swagger/api versioning

--- src/Template.Api/Startup.cs
+++ src/Template.Api/Startup.cs
@@ -1,25 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using AutoMapper;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using Template.Domain.V1.Implementation;
 using Template.Domain.V1.Interfaces;
 using Template.Repositories.Abstractions.Interfaces;
 using Template.Repositories.Example.Implementation;
 using Template.Services.Abstractions.Interfaces;
 using Template.Services.Example.Implementation;
+using Template.Swagger;
 
 namespace Template.Api
 {
     public sealed class Startup
     {
-        private IConfiguration _configuration;
+        private const string ApiVersioningKey = "Api:Versioning";
+        private const string ApiExplorerKey = "Api:Explorer";
+
+        private readonly IConfiguration _configuration;
 
         private static IEnumerable<Assembly> Assemblies => AppDomain.CurrentDomain.GetAssemblies();
 
@@ -37,12 +46,35 @@ namespace Template.Api
 
             services.AddAutoMapper(Assemblies);
 
+            services.AddApiVersioning(options => _configuration.GetSection(ApiVersioningKey).Get<ApiVersioningOptions>());
+
+            services.AddVersionedApiExplorer(options => _configuration.GetSection(ApiExplorerKey).Get<ApiExplorerOptions>());
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.ResolveConflictingActions(apiDescription => apiDescription.First());
+            });
+
             services.AddMvc().AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblies(Assemblies));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider versionDescriptionProvider)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var apiVersionDescription in versionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{apiVersionDescription.GroupName}/swagger.json", apiVersionDescription.GroupName.ToUpperInvariant());
+                }
+
+                options.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
